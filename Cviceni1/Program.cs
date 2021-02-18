456e93a69ceeff2415d91f44b5abe0819a3bd7f6
Daniel Pichnarcik <pichnarcikdani3l@gmail.com>
2021-02-18 10:03:56 +0100

    ukol2

--- Cviceni1/Program.cs
+++ Cviceni1/Program.cs
@@ -6,6 +6,11 @@ namespace Cviceni1
     {
         private static void Main(string[] args)
         {
+            vypisUkol2();
+        }
+
+        public static void vypisUkol1()
+        {
             string[] @pole = { "Josef", "Dan", "Michal" };
             string txt = "Josef Novák\nJindrišská 16\n111 50, Praha 1\n";
             Console.WriteLine(txt);
@@ -14,6 +19,38 @@ namespace Cviceni1
             {
                 Console.Write($"{pole[i]}\nJindrišská 16\n111 50, Praha 1\n\n");
             }
+            Console.ReadKey();
+        }
+
+        public static void vypisUkol2()
+        {
+            Console.WriteLine("for abeceda");
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(Convert.ToChar(65 + i) + ",");
+            }
+
+            int j = 0;
+            Console.WriteLine("\n\nwhile abeceda");
+            while (true)
+            {
+                if (j > 25)
+                    break;
+
+                Console.Write(Convert.ToChar(65 + j) + ",");
+                j++;
+            }
+
+            j = 0;
+            Console.WriteLine("\n\ndo while abeceda");
+            do
+            {
+                Console.Write(Convert.ToChar(65 + j) + ",");
+                j++;
+            }
+            while (j < 26);
+
+            Console.ReadKey();
         }
     }
 }
