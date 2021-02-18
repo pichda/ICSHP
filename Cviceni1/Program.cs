4a7f348e73ce63af86c10b0b3ff771d284036f4c
Daniel Pichnarcik <pichnarcikdani3l@gmail.com>
2021-02-18 09:16:26 +0100

    Add project files.

---
+++ Cviceni1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cviceni1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
