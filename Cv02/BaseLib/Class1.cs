07a8df685ef82490417e6330b952ed42900a016d
Daniel Pichnarcik <pichnarcikdani3l@gmail.com>
2021-02-25 11:36:02 +0100

    cviceni02 a zmena struktury

---
+++ Cv02/BaseLib/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fei
+{
+    namespace BaseLib
+    {
+        public class Reading
+        {
+            /// <summary>
+            /// Cte uzivatelsky vstup a vrati double. Pokud je vstup spatny, vrati 0.0
+            /// </summary>
+            /// <returns></returns>
+            static public double ReadDouble(string msg)
+            {
+                double vstup;
+                try
+                {
+                    vstup = Convert.ToDouble(Console.ReadLine());
+                    return vstup;
+                }
+                catch
+                {
+                    return 0.0;
+                }
+            }
+
+            /// <summary>
+            /// Cte uzivatelsky vstup a vrati int. Pokud je vstup spatny, vrati 0
+            /// </summary>
+            /// <returns></returns>
+            static public int ReadInt()
+            {
+                int vstup;
+                try
+                {
+                    vstup = Convert.ToInt32(Console.ReadLine());
+                    return vstup;
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+
+            /// <summary>
+            /// Precte prvni znak od uzivatele a vrati char.
+            /// </summary>
+            /// <returns></returns>
+            static public char ReadChar()
+            {
+                Console.WriteLine("Zadej znak");
+                char c = Convert.ToChar(Console.Read());
+                return c;
+            }
+
+            /// <summary>
+            /// Precte cely radek a vrati String.
+            /// </summary>
+            /// <returns></returns>
+            static public string ReadString()
+            {
+                String input = Console.ReadLine();
+                return input;
+            }
+        }
+    }
+}
---
+++ Cv02/PraceSPoli/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraceSPoli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[] pole = new int[10];
+            int index = 0;
+            char vstup;
+
+            while (true)
+            {
+                Console.WriteLine("--------------------------------------------");
+                Console.WriteLine("1.Zadaní prvků pole z klávesnice");
+                Console.WriteLine("2.Výpis pole na obrazovku");
+                Console.WriteLine("3.Utřídění pole vzestupně");
+                Console.WriteLine("4.Utřídění pole vzestupně");
+                Console.WriteLine("5.Hledání minimálního prvku");
+                Console.WriteLine("6.Hledání prvního výskytu zadaného čísla");
+                Console.WriteLine("7.Hledání posledního výskytu zadaného čísla");
+                Console.WriteLine("8.Konec programu");
+                Console.WriteLine("--------------------------------------------");
+
+                try
+                {
+                    vstup = Convert.ToChar(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    vstup = '0';
+                }
+
+                switch (vstup)
+                {
+                    case '1':
+                        while (index < 10)
+                        {
+                            Console.WriteLine("Zadej číslo.");
+                            pole[index] = Fei.BaseLib.Reading.ReadInt();
+                            index++;
+                        }
+                        break;
+
+                    case '2':
+                        for (int i = 0; i < index; i++)
+                        {
+                            Console.WriteLine(pole[i]);
+                        }
+                        break;
+
+                    case '3':
+                        Array.Sort(pole);
+                        break;
+
+                    case '4':
+                        Array.Sort(pole);
+                        Array.Reverse(pole);
+                        break;
+
+                    case '5':
+                        Console.WriteLine(pole.Min(prvek => prvek));
+                        break;
+
+                    case '6':
+                        Console.WriteLine("Zadej hledane cislo:");
+                        int hledane;
+                        try
+                        {
+                            hledane = Convert.ToInt32(Console.ReadLine());
+                            for (int i = 0; i < index; i++)
+                            {
+                                if (pole[i] == hledane)
+                                {
+                                    Console.WriteLine($"První výskyt čísla je:{i}");
+                                }
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Špatně zadané číslo");
+                        }
+                        break;
+
+                    case '7':
+                        int pozice = 0;
+                        Console.WriteLine("Zadej hledane cislo:");
+                        int hledaneCislo;
+                        try
+                        {
+                            hledaneCislo = Convert.ToInt32(Console.ReadLine());
+                            for (int i = 0; i < index; i++)
+                            {
+                                if (pole[i] == hledaneCislo)
+                                {
+                                    pozice = i;
+                                }
+                            }
+                            Console.WriteLine($"Poslední výskyt čísla je:{pozice}");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Špatně zadané číslo");
+                        }
+                        break;
+
+                    case '8':
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        Console.WriteLine($"Špatný vstup:{vstup}");
+                        break;
+                }
+            }
+        }
+    }
+}
