a15e51c249d89dbf00dd2e86f36bf6f6576f7512
Daniel Pichnarcik <pichnarcikdani3l@gmail.com>
2021-04-21 23:26:14 +0200

    cv08
    test TestSimpleNumericSortedRange() - failed, i kdyz jsou prvky serazene

---
+++ Cv08/Genericita/Genericita/MinMaxHashTable.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericExercise.Tests
+{
+
+    class MinMaxHashTable<TKey, TValue> where TKey : IComparable
+    {
+        internal struct Prvek<TKey, TValue>
+        {
+            public TKey Key { get; set; }
+            public TValue Value { get; set; }
+        }
+
+        public int Count { get; set; }
+        public LinkedList<Prvek<TKey, TValue>>[] items;
+        private int size;
+        private int maximum;
+        private int minimum;
+
+        
+
+
+        public int Maximum
+        {
+            get { if (Count != 0) { return maximum; } throw new InvalidOperationException(); }
+            set { maximum = value; }
+        }
+        public int Minimum
+        {
+            get { if (Count != 0) { return minimum; } throw new InvalidOperationException(); }
+            set { minimum = value; }
+        }
+
+        protected int GetArrayPosition(TKey key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+
+
+
+        public MinMaxHashTable(int capacity)
+        {
+            Count = 0;
+            this.items = new LinkedList<Prvek<TKey, TValue>>[capacity];
+            maximum = 0;
+            minimum = 0;
+            size = capacity;
+        }
+        public MinMaxHashTable()
+        {
+            Count = 0;
+            this.items = new LinkedList<Prvek<TKey, TValue>>[20];
+            maximum = 0;
+            minimum = 0;
+            size = 20;
+        }
+
+        public Prvek<TKey, TValue>[] this[int indexMin, int indexMax]
+        {
+            get
+            {
+                return Range(indexMin, indexMax);
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+
+            int pozice = GetArrayPosition(key);
+
+            LinkedList<Prvek<TKey, TValue>> linkedList = items[pozice];
+
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<Prvek<TKey, TValue>>();
+                items[pozice] = linkedList;
+            }
+
+            Prvek<TKey, TValue> prvek = new Prvek<TKey, TValue>() { Key = key, Value = value };
+
+            if (linkedList.Count == 0)
+            {
+                linkedList.AddFirst(prvek);
+            }
+            else
+            {
+                if (Contains(key))
+                {
+                    throw new ArgumentException();
+                }
+                items[pozice].AddFirst(prvek);
+            }
+
+            if (Convert.ToInt32(key) < minimum)
+            {
+                minimum = Convert.ToInt32(key);
+            }
+            if (Convert.ToInt32(key) > maximum)
+            {
+                maximum = Convert.ToInt32(key);
+            }
+            Count++;
+        }
+
+        public bool Contains(TKey key)
+        {
+            if (Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                int pozice = GetArrayPosition(key);
+
+                if (items[pozice] == null)
+                {
+                    return false;
+                }
+
+                LinkedListNode<Prvek<TKey, TValue>> aktualni = items[pozice].First;
+                while (aktualni!=null)
+                {
+                    if (aktualni.Value.Key.Equals(key))
+                    {
+                        return true;
+                    }
+                    aktualni = aktualni.Next;
+                }
+                return false;
+            }
+        }
+
+        public TValue Get(TKey key)
+        {
+
+            if (Count == 0)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            int pozice = GetArrayPosition(key);
+
+            if (items[pozice]==null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            LinkedListNode<Prvek<TKey, TValue>> aktualni = items[pozice].First;
+            while (aktualni != null)
+            {
+                if (aktualni.Value.Key.Equals(key))
+                {
+                    return aktualni.Value.Value;
+                }
+            }
+            throw new KeyNotFoundException();
+        }
+
+        public TValue Remove(TKey key)
+        {
+            if (Count == 0)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            int pozice = GetArrayPosition(key);
+
+            if (items[pozice]==null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            LinkedListNode<Prvek<TKey, TValue>> aktualni = items[pozice].First;
+            while (aktualni != null)
+            {
+                if (aktualni.Value.Key.Equals(key))
+                {
+                    TValue val = aktualni.Value.Value;
+                    items[pozice].Remove(aktualni);
+                    Count--;
+                    return val;
+                }
+            }
+            throw new KeyNotFoundException();
+
+        }
+
+        public Prvek<TKey, TValue>[] Range(int min, int max)
+        {
+            if (Count > 0)
+            {
+                int velikostRange = 0;
+                for (int i = min; i < max+1; i++)
+                {
+                    velikostRange += items[i].Count;
+                }
+
+                Prvek<TKey, TValue>[] range = new Prvek<TKey, TValue>[velikostRange];
+                int index = 0;
+
+                for (int i = min; i < max+1; i++)
+                {
+                    LinkedListNode<Prvek<TKey, TValue>> aktualni = items[i].First;
+                    while (aktualni != null)
+                    {
+                        range[index] = aktualni.Value;
+                        index++;
+                        aktualni = aktualni.Next;
+                    }
+                }
+                return range;
+            }
+            return null;
+        }
+
+        public Prvek<TKey, TValue>[] SortedRange(int min, int max)
+        {
+            Prvek<TKey, TValue>[] range = Range(min, max);
+
+            for (int i = 0; i < range.Length - 1; i++)
+            {
+                for (int j = 0; j < range.Length - i - 1; j++)
+                {
+                    if (range[j + 1].Key.CompareTo(range[j].Key) < 0)
+                    {
+                        Prvek<TKey, TValue> tmp = range[j + 1];
+                        range[j + 1] = range[j];
+                        range[j] = tmp;
+                    }
+                }
+            }
+            return range;
+        }
+    }
+}
---
+++ Cv08/Genericita/Genericita/MinMaxHashTableTests.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GenericExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GenericExercise.Tests
+{
+    [TestClass()]
+    public class MinMaxHashTableTests
+    {
+
+        #region Basic hash tests (add, contains, get)
+        [TestMethod()]
+        public void TestOfContainsOnEmptyHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            Assert.IsFalse(table.Contains(-1));
+            Assert.IsFalse(table.Contains(0));
+            Assert.IsFalse(table.Contains(1));
+        }
+
+        [TestMethod(), ExpectedException(typeof(KeyNotFoundException))]
+        public void TestOfGetOnEmptyHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            string value = table.Get(0);
+            // Exception should be thrown at previous line
+        }
+
+        [TestMethod()]
+        public void TestOfAddingSingleElementToHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            table.Add(1, "one");
+
+            Assert.IsTrue(table.Contains(1));
+            Assert.AreEqual("one", table.Get(1));
+            Assert.AreEqual(1, table.Count);
+        }
+
+        [TestMethod(), ExpectedException(typeof(ArgumentException))]
+        public void TestOfAddingDuplicicKey()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            table.Add(1, "one");
+            table.Add(1, "two");
+            // Exception should be thrown at previous line
+        }
+
+        [TestMethod()]
+        public void TestOfAddingMultipleDistinctElementToHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            table.Add(1, "one");
+            table.Add(2, "two");
+            table.Add(3, "three");
+
+            Assert.IsTrue(table.Contains(1));
+            Assert.AreEqual("one", table.Get(1));
+
+            Assert.IsTrue(table.Contains(2));
+            Assert.AreEqual("two", table.Get(2));
+
+            Assert.IsTrue(table.Contains(3));
+            Assert.AreEqual("three", table.Get(3));
+
+            Assert.AreEqual(3, table.Count);
+        }
+
+        [TestMethod()]
+        public void TestOfAddingManyDistinctElementToHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>(capacity: 16384);
+
+            const int elements = 1000000;
+            for (int i = 0; i < elements; i++)
+            {
+                table.Add(i, i.ToString());
+            }
+
+            Assert.AreEqual(elements, table.Count);
+
+            for (int i = 0; i < elements; i++)
+            {
+                Assert.IsTrue(table.Contains(i));
+                Assert.AreEqual(i.ToString(), table.Get(i));
+            }
+
+            Assert.IsFalse(table.Contains(-1));
+            Assert.IsFalse(table.Contains(elements));
+        }
+
+        [TestMethod()]
+        public void TestOfRemovingElementFromHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            table.Add(1, "one");
+            table.Add(2, "two");
+            table.Add(3, "three");
+
+
+            var value = table.Remove(2);
+
+
+            Assert.AreEqual("two", value);
+
+            Assert.IsTrue(table.Contains(1));
+            Assert.AreEqual("one", table.Get(1));
+
+            Assert.IsFalse(table.Contains(2));
+
+            Assert.IsTrue(table.Contains(3));
+            Assert.AreEqual("three", table.Get(3));
+
+            Assert.AreEqual(2, table.Count);
+        }
+
+        [TestMethod(), ExpectedException(typeof(KeyNotFoundException))]
+        public void TestOfRemovingNonExistentElementFromHashTable()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            table.Add(1, "one");
+            table.Add(2, "two");
+            table.Add(3, "three");
+
+
+            var value = table.Remove(20);
+            // Exception should be thrown at previous line
+        }
+        #endregion
+
+        #region Min-Max tests
+        [TestMethod(), ExpectedException(typeof(InvalidOperationException))]
+        public void TestOfEmptyHashTableMinValue()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            int min = table.Minimum;
+            // Exception should be thrown at previous line
+        }
+
+        [TestMethod(), ExpectedException(typeof(InvalidOperationException))]
+        public void TestOfEmptyHashTableMaxValue()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            int min = table.Maximum;
+            // Exception should be thrown at previous line
+        }
+
+        [TestMethod()]
+        public void TestOfNotEmptyHashTableMinMaxValues()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            table.Add(-1, "one");
+            table.Add(2, "two");
+            table.Add(-3, "three");
+
+            Assert.AreEqual(-3, table.Minimum);
+            Assert.AreEqual(2, table.Maximum);
+        }
+        #endregion
+
+        #region Range tests
+        [TestMethod()]
+        public void TestSimpleNumericRangeUsingIndexer()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            const int elements = 10;
+            for (int i = 0; i < elements; i++)
+            {
+                table.Add(i, i.ToString());
+            }
+
+
+            var range = table[2, 4];
+
+
+            int[] remaining = new int[] { 0, 0, 1, 1, 1, 0, 0, 0, 0, 0 };
+
+            foreach (var item in range)
+            {
+                if (remaining[item.Key] < 0)
+                {
+                    Assert.Fail("Unexpected element in range query");
+                }
+
+                remaining[item.Key]--;
+            }
+
+            foreach (var item in remaining)
+            {
+                if (item > 0)
+                {
+                    Assert.Fail("Some elements weren't read from range");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void TestSimpleNumericSortedRange()
+        {
+            MinMaxHashTable<int, string> table = new MinMaxHashTable<int, string>();
+
+            const int elements = 100;
+            for (int i = 0; i < elements; i++)
+            {
+                int keyvalue = i - 50;
+                table.Add(keyvalue, keyvalue.ToString());
+            }
+
+
+            var range = table.SortedRange(13, 17).Select(kv => kv.Key);
+
+
+            CollectionAssert.AreEqual(
+                new List<int>() { 13, 14, 15, 16, 17},
+                range.ToList());
+        }
+        #endregion
+    }
+}
